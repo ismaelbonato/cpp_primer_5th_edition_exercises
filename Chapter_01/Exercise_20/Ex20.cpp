170ff13493d31892cc38352dc451ede736b87755
Ismael Bonato <ismaelbonato@gmail.com>
2023-05-31 21:48:28 -0300

    fixing the name of Ex35 folder

--- Chapter_01/Exercise_20/Ex20.cpp
+++ Chapter_01/Exercise_20/Ex20.cpp
@@ -1,15 +1,14 @@
-#include <iostream>
 #include "Sales_item.h"
+#include <iostream>
 
 int main()
 {
     Sales_item item;
 
-    while(std::cin >> item) {
+    while (std::cin >> item) {
         std::cout << item << std::endl;
     }
     return 0;
 }
-
 
 // Run ./a.out <input
